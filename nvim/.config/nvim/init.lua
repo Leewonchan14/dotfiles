0840e30111dd020eff0e405013801d426d255bbe
Leewonchan <twoone14@tukorea.ac.kr>
2025-11-09 17:58:49 +0900

    add nvim LSP

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require("config.lazy").setup({
+  extras = {
+    "lazyvim.plugins.extras.lang.lua",
+    "lazyvim.plugins.extras.lang.python",
+  },
+})
