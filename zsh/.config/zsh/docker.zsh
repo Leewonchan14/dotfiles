7b1271072d0ef8fb3745ac8fdaa7975c0a992d55
Leewonchan <twoone14@tukorea.ac.kr>
2026-02-12 08:25:20 +0900

    feat: add docker compose detail alias

--- zsh/.config/zsh/docker.zsh
+++ zsh/.config/zsh/docker.zsh
@@ -1,6 +1,9 @@
 source <(docker completion zsh)
 
 alias dc='docker compose'
+alias dcl='docker compose logs -f --tail 100'
+alias dcu='docker compose up -d'
+alias dcr='docker compose restart'
+alias dce='docker compose exec -it'
 alias dsta='docker ps -q | xargs --no-run-if-empty docker stop'
 alias dcsta="dc ps -q | xargs --no-run-if-empty docker stop"
-alias -g ex="exec -it"
